276437ce75c8d320c680b1a711cddbb147de44ca
rom61977 <rom61977@gmail.com>
2023-12-21 19:01:30 +0900

    test.js update

--- tests/test.js
+++ tests/test.js
@@ -7,33 +7,31 @@ import { assertExists, exists, isElement } from "../dist/app/utils/typeGuards.js
 import userAgent from "../dist/app/utils/userAgents.js";
 import * as vldt from "../dist/app/utils/validator.js";
 
-const source = {
-	name: "moedb",
-	rootUrl: "https://www.moedb.net/",
-	entryUrl: "https://www.moedb.net/keywords/%E8%BF%91%E8%A6%AA%E7%9B%B8%E5%A7%A6?page=279",
+const enigma = {
+	name: "enigma",
+	rootUrl: "https://enigma2.ahoseek.com/",
+	entryUrl: "https://enigma2.ahoseek.com/",
 	language: "JP",
 	saveDir: "./exports",
 	logDir: "./logs",
 	siteType: "links",
 	nextPageType: "last",
-	nextPageLinkSelector: "ul.paginali.page-item:nth-child(7) > a:nth-child(1)tion > li.last > a",
+	nextPageLinkSelector: "ul.pagination > li.last > a",
 
-	tagFiltering: true,
+	tagFiltering: false,
 	tagCollect: true,
-	tags: ["妹", "姉", "兄", "弟", "姉妹", "兄妹", "姉弟"],
-	indexTagSelector: "ul.list-inline > li.list-inline-item > a",
-	articleTagSelector: "div.article-text > ul.list-inline > li.list-inline-item > a",
+	articleTagSelector: "span.category > a",
 
-	indexLinkBlockSelector: "div.article-summary",
-	indexLinkSelector: "div.article-summary__more > a",
+	indexLinkBlockSelector: "div.entry-card-content",
+	indexLinkSelector: "p.entry-read > a.entry-read-link",
 
-	articleTitleSelector: "h1.card-header",
-	articleBodySelector: "div#the-content.article-text > p:nth-child(3)",
+	articleTitleSelector: "h1.entry-title",
+	articleBodySelector: "div#the-content",
 
-	lastUrlSelector: "li.page-item:nth-child(8) > a:nth-child(1)",
-	lastPageNumberRegExp: "=(\\d+)$",
+	lastUrlSelector: "ul.pagination > li.last > a",
+	lastPageNumberRegExp: "/page/(\\d+)/$",
 };
 
-const scr = new Scraper(source);
+const scr = new Scraper(enigma);
 
 scr.getSiteURLs();
