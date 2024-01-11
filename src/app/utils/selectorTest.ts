f1e035d3df84834bfc159fd268d2bff9ae0c78a4
rom61977 <rom61977@gmail.com>
2024-01-12 08:08:04 +0900

    types

--- src/app/utils/selectorTest.ts
+++ src/app/utils/selectorTest.ts
@@ -26,11 +26,29 @@ type extractTypes = "link" | "text";
 
 
 type result = {
-	pass: boolean;
-	returned: string | Element | Node;
+	pass: true;
+	returned: string;
+} | {
+	pass: false;
+	error: string;
 };
+
+type testResult<T> = {
+	[K in keyof T]: result;
+};
+
+type selectors = Pick<site, 'lastUrlSelector' | 'lastPageNumberRegExp' | 'nextPageLinkSelector' |
+	'nextPageUrlRegExp' | 'indexTagSelector' | 'indexLinkBlockSelector' | 'indexLinkSelector' | 'articleTagSelector' |
+	'articleBlockSelector' | 'articleTitleSelector' | 'articleBodySelector'>;
+
+const tr = <testResult<selectors>>{};
+tr.articleBlockSelector = {
+	pass: false
+}
+
+/*
 interface Selectors {
-	lastUrlSelector: result;
+lastUrlSelector: result;
 	lastPageNumberRegExp: result;
 
 	nextPageLinkSelector: result;
@@ -46,141 +64,175 @@ interface Selectors {
 	articleTitleSelector: result;
 	articleBodySelector: result;
 }
-
+*/
 
 export default async function validateSelectors(site: site): Promise<object> {
 
-	const testResults = <Selectors>{};
+	const testResults = <testResult<selectors>>{};
 
 	const indexDom = await getDom(site.entryUrl);
 
 	if (site.nextPageType === "last" && site.lastUrlSelector) {
-		extract("link", indexDom, "lastUrlSelector");
+		testResults.lastUrlSelector = extract("link", indexDom, "lastUrlSelector");
 
 		if (testResults.lastUrlSelector.pass && site.lastPageNumberRegExp) {
-			const res = <result>{};
 			const regexp = new RegExp(site.lastPageNumberRegExp);
-			const matched = (testResults.lastUrlSelector.returned as string).match(regexp);
-			res.pass = matched && Number(matched[1]) ? true : false;
-			res.returned = matched && Number(matched[1]) ? matched[1] : "RegExp returned null";
-			testResults.lastPageNumberRegExp = res;
+			const matched = (testResults.lastUrlSelector.returned).match(regexp);
+			if (matched) {
+				testResults.lastPageNumberRegExp = {
+					pass: true,
+					returned: matched[1],
+				};
+			} else {
+
+				testResults.lastPageNumberRegExp = {
+					pass: false,
+					error: "RegExp returned null"
+				}
+			}
 		}
-	}
 
-	if (site.indexLinkBlockSelector) {
-		selectorAll(indexDom, "indexLinkBlockSelector");
+		if (site.indexLinkBlockSelector) {
+			testResults.indexLinkBlockSelector = extract("text", indexDom, "indexLinkBlockSelector", "all");
 
-		if (testResults.indexLinkBlockSelector.pass) {
-			const linkElem = testResults.indexLinkBlockSelector.returned;
+			if (testResults.indexLinkBlockSelector.pass) {
+				const linkElem = getElement(indexDom, "indexLinkBlockSelector");
+				if (linkElem) testResults.indexLinkSelector = extract("link", linkElem, "indexLinkSelector");
 
-			extract("link", linkElem, "indexLinkSelector");
-
-			if (site.indexTagSelector) {
-				selectorAll(linkElem, "indexTagSelector");
-				if (testResults.indexTagSelector.pass && testResults.indexTagSelector.returned instanceof Element) {
-					testResults.indexTagSelector.returned = testResults.indexTagSelector.returned.textContent;
+				if (site.indexTagSelector) {
+					if (linkElem) testResults.indexTagSelector = extract("link", linkElem, "indexTagSelector", "all");
 
 				}
 			}
-		} else {
-			throw testResults;
 		}
-	}
 
-	//article test
-	const articleUrl = site.siteType === "links" ? testResults.indexLinkSelector.returned : site.entryUrl;
-	let articleDom: Document | Element = await getDom(articleUrl);
+		//article test
+		const articleUrl = site.siteType === "links" ? testResults.indexLinkSelector.returned : site.entryUrl;
+		let articleDom: Document | Element = await getDom(articleUrl);
 
-	if (site.articleBlockSelector) {
-		selectorAll(articleDom, "articleBlockSelector");
-		if (testResults.articleBlockSelector.returned instanceof Element) {
-			articleDom = testResults.articleBlockSelector.returned;
-		} else {
-			throw testResults;
-		}
-	}
-
-	extract("text", articleDom, "articleTitleSelector");
-	extract("text", articleDom, "articleBodySelector");
-
-	if (site.articleTagSelector) {
-		selectorAll(articleDom, "articleTagSelector");
-		if (testResults.articleTagSelector.pass && testResults.articleBlockSelector.returned instanceof Element) {
-			const text = testResults.articleBlockSelector.returned.textContent;
-			if (text) {
-				testResults.articleBlockSelector.returned = text;
-				testResults.articleBlockSelector.pass = true;
+		if (site.articleBlockSelector) {
+			selectorAll(articleDom, "articleBlockSelector");
+			if (testResults.articleBlockSelector.returned instanceof Element) {
+				articleDom = testResults.articleBlockSelector.returned;
 			} else {
-
-				testResults.articleBlockSelector.returned = "Failed to get textContent from querySelectorAll result.";
-				testResults.articleBlockSelector.pass = true;
+				throw testResults;
 			}
 		}
-	}
 
-	return testResults;
+		extract("text", articleDom, "articleTitleSelector");
+		extract("text", articleDom, "articleBodySelector");
 
+		if (site.articleTagSelector) {
+			selectorAll(articleDom, "articleTagSelector");
+			if (testResults.articleTagSelector.pass && testResults.articleBlockSelector.returned instanceof Element) {
+				const text = testResults.articleBlockSelector.returned.textContent;
+				if (text) {
+					testResults.articleBlockSelector.returned = text;
+					testResults.articleBlockSelector.pass = true;
+				} else {
 
-	async function getDom(url: string): Promise<Document> {
-		let dom: Document;
-
-		const loader = new ResourceLoader({
-			userAgent: userAgent(),
-		});
-
-		try {
-			const jd = await JSDOM.fromURL(url, { resources: loader });
-			dom = jd.window.document;
-		} catch (err) {
-			throw new Error(`JSDOM failed on ${site.entryUrl}\n ${err}`);
+					testResults.articleBlockSelector.returned = "Failed to get textContent from querySelectorAll result.";
+					testResults.articleBlockSelector.pass = true;
+				}
+			}
 		}
-		return dom;
 
-	}
-
-	function extract(type: extractTypes, elem: Element | Document, selectorName: string): void {
+		return testResults;
 
 
-		const selector = getSelector(site, selectorName);
-		const res = <result>{};
+		async function getDom(url: string): Promise<Document> {
+			let dom: Document;
 
-		const el = elem.querySelector(selector);
-		if (el) {
+			const loader = new ResourceLoader({
+				userAgent: userAgent(),
+			});
+
+			try {
+				const jd = await JSDOM.fromURL(url, { resources: loader });
+				dom = jd.window.document;
+			} catch (err) {
+				throw new Error(`JSDOM failed on ${site.entryUrl}\n ${err}`);
+			}
+			return dom;
+
+		}
+
+		function getElement(
+			elem: Element | Document, selectorName: keyof selectors
+		): Element | false {
+			const selector = getSelector(site, selectorName);
+			const el = elem.querySelectorAll(selector);
+			return el ? el.item(0) : false;
+		}
+
+
+
+		function extract(type: extractTypes, elem: Element | Document, selectorName: keyof selectors, mode = 'single'): result {
+
+			const resultFailed: result = {
+				pass: false,
+				error: ""
+			};
+			const selector = getSelector(site, selectorName);
+			let el: Element;
+			if (mode === 'all') {
+				const ele = elem.querySelectorAll(selector);
+				if (ele.length === 0) {
+					resultFailed.error = `querySelectorAll failed on ${selector}`;
+					return resultFailed;
+				}
+				el = ele.item(0);
+			} else {
+				const ele = elem.querySelector(selector);
+				if (!ele) {
+					resultFailed.error = `querySelector failed on ${selector}`;
+					return resultFailed;
+				}
+				el = ele;
+			}
 			const extracted = type === "link" ? el.getAttribute("href") : el.textContent;
-			res.returned = extracted ? extracted : `${selector} failed to extract ${type}.`;
-			res.pass = res.returned === extracted ? true : false;
-		} else {
+
+			if (!extracted) {
+				return {
+					pass: false,
+					error: `Failed to get ${type} from Element acquired from ${selector}`
+				}
+			}
+			return {
+				pass: true,
+				returned: extracted,
+			};
+
+		}
+
+		function selectorAll(elem: Document | Element, selectorName: string): void {
+
+			const selector = getSelector(site, selectorName);
+			const nList = elem.querySelectorAll(selector);
+			const res = <result>{};
+
+			res.returned = nList.length > 0 ? nList.item(0) : `${selector} failed to extract valid nodeLists.`;
+			res.pass = typeof res.returned !== 'string' ? true : false;
+			testResults[selectorName as keyof typeof testResults] = res;
+		}
+
+		function getSelector(site: site, selectorName: keyof site): string {
+
+			const selector = site[selectorName];
+
+			if (selector && typeof selector === 'string') {
+				return selector;
+			}
+
+			const res = <result>{};
 			res.pass = false;
-			res.returned = `querySelector failed on selector ${selector}`;
-		}
-
-	}
-
-	function selectorAll(elem: Document | Element, selectorName: string): void {
-
-		const selector = getSelector(site, selectorName);
-		const nList = elem.querySelectorAll(selector);
-		const res = <result>{};
-
-		res.returned = nList.length > 0 ? nList.item(0) : `${selector} failed to extract valid nodeLists.`;
-		res.pass = typeof res.returned !== 'string' ? true : false;
-		testResults[selectorName as keyof typeof testResults] = res;
-	}
-
-	function getSelector(site: site, selectorName: string): string {
-
-		const selector = site[selectorName as keyof typeof site];
-
-		if (selector && typeof selector === 'string') {
-			return selector;
-		}
-
-		throw new Error(`selectorName ${selectorName} not found in site\n site=${site}`);
-
-	}
-	/**
-		function outputResult(site: site, testResult: Selectors) {
+			res.error = `selectorName ${selectorName} not found in site\n site=${site}`;
+			testResults[selectorName] = res;
 
 		}
-	*/
-}
+		/**
+			function outputResult(site: site, testResult: Selectors) {
+
+			}
+		*/
+	}
